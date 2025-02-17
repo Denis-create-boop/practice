270af2d0082d123a3b1b2b79b790b5587d900069
Denis-create-boop <den_1994_2015@mail.ru>
2025-02-17 11:42:54 +0300

    -

--- Генератор паролей/main.py
+++ Генератор паролей/main.py
@@ -1,63 +1,75 @@
-from random import *
-print('введите длинну желаемого пароля')
-x = int(input())
-print('включать ли цыфры в пароль?')
-ans = input()
-print('включать ли заглавные буквы?')
-up = input()
-print('включать ли строчные буквы?')
-a = input()
-print('включать ли символы?')
-sym = input()
-chars = ''
 
-def length(num):
-    if len(chars1) == num:
+import random
+
+
+def elements_in_pass(lenth, param):
+    elements = ''
+    for _ in range(lenth):
+        elements += random.choice(param)
+    return elements
+
+
+def check(question, answer):
+    if answer.lower() in ["да", "д"]:
         return True
-    else:
+    elif answer.lower() in ["нет", "не", "н"]:
         return False
+    else:
+        print("Введены неккоректные данные")
+        print(question)
+        answer = input()
+        return check(question, answer)
 
-def upp(num):
-    chars = ''
-    for i in range(num):
-        chars += choice('QWERTYUIOPLKJHGFDSAZXCVBNM')
-    return chars
+def main():
+    while True:
+        result = ''
 
-def low(num):
-    chars = ''
-    for i in range(num):
-        chars += choice('qwertyuioplkjhgfdsazxcvbnm')
-    return chars
+        while True:
+            # длина пароля
+            lenth_pass = input("Введите длину пароля: ")
+            try:
+                lenth_pass = int(lenth_pass)
+                break
+            except:
+                print('Введите число')
 
-def dig(num):
-    chars = ''
-    for i in range(num):
-        chars += choice('1234567890')
-    return chars
+        # включать ли цыфры
+        question = 'Включать ли цифры в пароль?'
+        print(question)
+        nums_pass = input()
+        if check(question, nums_pass):
+            result += elements_in_pass(lenth_pass, "1234567890")
 
-def punct(num):
-    chars = ''
-    for i in range(num):
-        chars += choice('!/?._-')
-    return chars
+        # включать ли заглавные
+        question = 'Включать ли заглавные буквы в пароль?'
+        print(question)
+        upper_letters = input()
+        if check(question, upper_letters):
+            result += elements_in_pass(lenth_pass, "QWERTYUIOPASDFGHJKLZXCVBNM")
 
-if ans == 'да':
-    chars += dig(x)
+        # включать ли строчные
+        question = 'Включать ли строчные буквы в пароль?'
+        print(question)
+        lower_letters = input()
+        if check(question, lower_letters):
+            result += elements_in_pass(lenth_pass, "qwertyuiopasdfghjklzxcvbnm")
 
-if up == 'да':
-    chars += upp(x)
+        # включать ли символы
+        question = 'Включать ли символы в пароль?'
+        print(question)
+        sym_pass = input()
+        if check(question, sym_pass):
+            result += elements_in_pass(lenth_pass, ".,_-?!")
 
-if sym == 'да':
-    chars += punct(x)
+        password = random.sample(result, lenth_pass)
 
-if a == 'да':
-    chars += low(x)
+        print(f"Пароль сгенерирован, ваш пароль: {''.join(password)}")
+        question = "Хотите сгенерировать новый пароль? да/нет"
+        print(question)
+        answer = input()
+        if check(question, answer) == False:
+            break
 
-chars1 = sample(chars, x)
-length(x)
-print('введите количество паролей')
-x1 = int(input())
-for i in range(x1):
-    chars2 = sample(chars, x)
-    print(*chars2, end='')
-    print()
+
+if __name__ == "__main__":
+    main()
